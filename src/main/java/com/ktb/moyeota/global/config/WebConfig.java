505bb5324df763e7c25651784c2bf31e8e9dc173
yeobi <kimhyjjang@gmail.com>
2026-09-22 14:09:38 +0900

    feat: 업로드 주체 인자 리졸버 추가

--- src/main/java/com/ktb/moyeota/global/config/WebConfig.java
+++ src/main/java/com/ktb/moyeota/global/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.ktb.moyeota.global.config;
 
 import com.ktb.moyeota.global.security.resolver.AuthUserArgumentResolver;
 import com.ktb.moyeota.global.security.resolver.SignupPrincipalArgumentResolver;
+import com.ktb.moyeota.global.security.resolver.UploadScopeArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -14,10 +15,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final AuthUserArgumentResolver authUserArgumentResolver;
     private final SignupPrincipalArgumentResolver signupPrincipalArgumentResolver;
+    private final UploadScopeArgumentResolver uploadScopeArgumentResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authUserArgumentResolver);
         resolvers.add(signupPrincipalArgumentResolver);
+        resolvers.add(uploadScopeArgumentResolver);
     }
 }
