9c022c9cfac63d43d837a30dbb9ff0c4fb31cfa8
Woorim <yiurimone@gmail.com>
2026-09-23 18:54:23 +0900

    fix: 컨트롤러 요청 경로에 /api 프리픽스 추가

--- src/main/java/com/ktb/moyeota/domain/community/controller/HomeController.java
+++ src/main/java/com/ktb/moyeota/domain/community/controller/HomeController.java
@@ -10,10 +10,12 @@ import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class HomeController {
 
